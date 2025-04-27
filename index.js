dce92c168ce50e83a52902efe25938b8d29f297a
unknown <felipe.silva2@mail.udp.cl>
2025-04-27 01:34:00 -0400

    port actualizado

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ import express from 'express';
 
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 app.get('/', (req,res) =>{
     res.send('Hello World!')
 })
 
-app.listen(5000, () =>{
+app.listen(port, () =>{
     console.log('server actrive on port 5000')
     console.log('http://localhost:5000')
 })
